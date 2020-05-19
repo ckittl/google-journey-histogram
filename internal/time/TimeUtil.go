846e8970b49843e708c185a9c221265388cfcd19
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-05-19 18:09:24 +0200

    Determine the day type of a date

--- internal/time/TimeUtil.go
+++ internal/time/TimeUtil.go
@@ -3,7 +3,33 @@ package time
 import (
 	"regexp"
 	"strconv"
+	gotime "time"
 )
+
+type DayType int
+
+const (
+	WorkingDay DayType = iota
+	WeekEnd
+)
+
+// Determine, whether its a working day or a weekend
+func GetDayType(time gotime.Time) DayType {
+	weekDay := time.Weekday()
+	if weekDay == gotime.Sunday || weekDay == gotime.Saturday {
+		return WorkingDay
+	} else {
+		return WeekEnd
+	}
+}
+
+func (d DayType) String() string {
+	if d == WorkingDay {
+		return "WorkingDay"
+	} else {
+		return "WeekEnd"
+	}
+}
 
 // Calculates the travel duration in minutes based on a string of the format 'X hours Y minutes'. The hours part is
 // optional
